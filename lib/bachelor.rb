6c8242afa4a76de73cd172cd9d57b57beb74f740
Kat Bartels <k@gothfem.me>
2018-08-21 15:05:51 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,8 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants, contestant|
+    contestant
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
